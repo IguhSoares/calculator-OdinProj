b9df9c21d8e7406df0f89ca4a14965bb153de998
IguhSoares <iguhsoares@gmail.com>
2022-10-25 14:26:33 -0300

    fix: regex was not catching subtraction sign
    hyphen character now being properly scaped

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,8 +1,8 @@
 // import buffer from './buffer.js';
 const { buffer } = require('./buffer');
 
-const FIRST_NUM_REGEX = /^(\d+\.?\d+)[+|-|÷|×]$/;
-const SECOND_NUM_REGEX = /^\d+\.?\d+[+|-|÷|×](\d+\.?\d+)$/;
+const FIRST_NUM_REGEX = /^(\d+\.?\d+)[+|\-|÷|×]$/;
+const SECOND_NUM_REGEX = /^\d+\.?\d+[+|\-|÷|×](\d+\.?\d+)$/;
 Object.freeze(FIRST_NUM_REGEX);
 Object.freeze(SECOND_NUM_REGEX);
 
