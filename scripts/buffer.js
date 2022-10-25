cced560f0f4da5c03e565e2b7d4ad0e965870ee1
IguhSoares <iguhsoares@gmail.com>
2022-10-25 13:25:43 -0300

    feat: move buffer object to a separate module

---
+++ scripts/buffer.js
@@ -0,0 +1,52 @@
+const buffer = {
+  number: null,
+  op: null,
+  calc(x) {
+    const operations = {
+      '+': n => this.num + n,
+      '-': n => this.num - n,
+      '*': n => this.num * n,
+      '/': n => this.num / n,
+    };
+    const isFloat = n => n % 1 !== 0;
+
+    if (this.operator === '/' && x === 0)
+      throw new Error('Division by zero attempt');
+
+    let result = operations[this.operator](x);
+    if (isFloat(result)) {
+      result = parseFloat(result.toFixed(15));
+    }
+    return result;
+  },
+  get num() {
+    return this.number;
+  },
+  set num(n) {
+    if (n === undefined)
+      throw new TypeError('Number cannot be set to undefined');
+    if (n !== null && typeof n !== 'number')
+      throw new TypeError('Not a number');
+    this.number = n;
+  },
+  get operator() {
+    return this.op;
+  },
+  set operator(oprt) {
+    if (this.number === null)
+      throw new Error('Unable to set operator: no number in buffer');
+
+    const valid = { '+': 1, '-': 1, '*': 1, '/': 1, null: 1 };
+    if (valid[oprt] === undefined)
+      throw new ReferenceError(`Invalid operator: ${oprt}`);
+
+    this.op = oprt;
+  },
+  clear() {
+    this.operator = null;
+    this.num = null;
+  },
+};
+
+export default buffer; /** ES6 export */
+// module.exports = { buffer }; /** Node export, used to run jest tests */
--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,18 +1,1 @@
-const buffer = {
-  num: null,
-  operator: null,
-  calc(x) {
-    const operations = {
-      '+': n => this.num + n,
-      '-': n => this.num - n,
-      '*': n => this.num * n,
-      '/': n => this.num / n,
-    };
-
-    return operations[this.operator](x);
-  },
-};
-
-module.exports = {
-  buffer,
-};
+import buffer from './buffer.js';
