1d2e391879b05cd6c9e09b48618003eab74a51dc
IguhSoares <iguhsoares@gmail.com>
2022-10-25 15:19:36 -0300

    feat: implement writeToDisplay, addToBuffer, calculate

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,8 +1,9 @@
 // import buffer from './buffer.js';
 const { buffer } = require('./buffer');
 
-const FIRST_NUM_REGEX = /^(\d+\.?\d+)[+|\-|÷|×]$/;
-const SECOND_NUM_REGEX = /^\d+\.?\d+[+|\-|÷|×](\d+\.?\d+)$/;
+const FIRST_NUM_REGEX = /^(\-?\d+\.?\d+)[+|\-|÷|×]$/;
+/** second number can never be negative, as the interface should not allow it */
+const SECOND_NUM_REGEX = /^\-?\d+\.?\d+[+|\-|÷|×](\d+\.?\d+)$/;
 Object.freeze(FIRST_NUM_REGEX);
 Object.freeze(SECOND_NUM_REGEX);
 
@@ -12,6 +13,26 @@ const getNumber = (display, position = 'first') => {
   return value.match(regex)[1];
 };
 
+const writeToDisplay = (display, result, op = null) => {
+  display.innerText = result + (op ? op : '');
+};
+
+const addToBuffer = (value, type = 'number') => {
+  if (type === 'number') buffer.num = Number(value);
+  else if (type === 'operator') buffer.operator = value;
+};
+
+const calculate = value => {
+  const result = buffer.calc(Number(value));
+  buffer.clear();
+  buffer.num = result;
+
+  return result;
+};
+
 module.exports = {
   getNumber,
+  writeToDisplay,
+  addToBuffer,
+  calculate,
 };
