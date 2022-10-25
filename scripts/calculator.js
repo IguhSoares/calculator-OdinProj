8c9c6005ab757ea1bc7c7b204166b77156999895
IguhSoares <iguhsoares@gmail.com>
2022-10-25 11:30:47 -0300

    feat: buffer.calc implementation

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,7 +1,16 @@
 const buffer = {
   num: null,
   operand: null,
-  calc: x => {},
+  calc(x) {
+    const operations = {
+      '+': n => this.num + n,
+      '-': n => this.num - n,
+      '*': n => this.num * n,
+      '/': n => this.num / n,
+    };
+
+    return operations[this.operand](x);
+  },
 };
 
 module.exports = {
