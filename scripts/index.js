8e1c3bf823982f40d658f8f27c01ccd112d67a00
IguhSoares <iguhsoares@gmail.com>
2022-10-26 22:14:39 -0300

    feat: implement methods that will be used on keys handlers:
    printToDisplay, updateOperator, backspace, addPoint, equalsTo

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,56 @@
-import {
-  calculate,
-  addToBuffer,
-  writeToDisplay,
-  getNumber,
-} from './calculator';
+// import {
+//   calculate,
+//   addToBuffer,
+//   writeToDisplay,
+//   getNumber,
+// } from './calculator';
+const { calculate, writeToDisplay, getNumber } = require('./calculator');
 
-const renderOnDisplay = () => {};
-const updateOperator = () => {};
-const backspace = () => {};
-const addPoint = () => {};
-const equalsTo = () => {};
+// const display = document.getElementById('display');
+
+const isOperator = val => ['+', '-', '*', '/'].includes(val);
+
+const printToDisplay = val => {
+  if (display.innerText.length >= 20) throw 'Max limit reached';
+  const itIsOperator = isOperator(val);
+  if (!itIsOperator || (itIsOperator && val === '-')) display.innerText += val;
+};
+
+const updateOperator = oprt => {
+  const displayText = display.innerText;
+  if (isOperator(displayText.at(-1))) {
+    display.innerText = displayText.slice(0, -1) + oprt;
+  }
+};
+
+const backspace = () => {
+  const displayText = display.innerText;
+  display.innerText = displayText.slice(0, -1);
+};
+
+const addPoint = () => {
+  const displayText = display.innerText;
+  let regex = /^\d+\.\d*[+|\-|×|÷]?(\d+\.\d+)?$/;
+  if (!regex.test(displayText)) {
+    regex = /^.+[+|\-|×|÷]$/;
+    display.innerText +=
+      displayText === '' || regex.test(displayText) ? '0.' : '.';
+  }
+};
+const equalsTo = () => {
+  const displayText = display.innerText;
+  if (/[+|\-|×|÷]$/.test(displayText)) backspace();
+  else {
+    const secondNum = getNumber(display, 'second');
+    if (secondNum) {
+      const result = calculate(secondNum);
+      writeToDisplay(display, result);
+    }
+  }
+};
 
 module.exports = {
-  renderOnDisplay,
+  printToDisplay,
   updateOperator,
   backspace,
   addPoint,
